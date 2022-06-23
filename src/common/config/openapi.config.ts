145d58c197aed98a8b0cfe12ac69ea552418848d
Dmytro Kostyshyn <dmytro.kostyshyn@magnise.com.ua>
2022-06-23 11:56:11 +0300

    Rename project from simple-tasks to simple-todo.

--- src/common/config/openapi.config.ts
+++ src/common/config/openapi.config.ts
@@ -1,11 +1,11 @@
 import { DocumentBuilder, SwaggerCustomOptions } from '@nestjs/swagger';
 
 export const documentOptions = new DocumentBuilder()
-  .setTitle('Simple-Tasks API')
-  .setDescription('The Simple-Tasks API')
+  .setTitle('Simple-Todo API')
+  .setDescription('The Simple-Todo API')
   .setVersion(process.env.npm_package_version)
   .build();
 
 export const swaggerOptions: SwaggerCustomOptions = {
-  customSiteTitle: 'Swagger Simple-Tasks API',
+  customSiteTitle: 'Swagger Simple-Todo API',
 };
