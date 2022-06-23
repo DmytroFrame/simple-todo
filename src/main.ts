d3a9c5b9ecb7e310c2ef4a335732348acddbc075
Dmytro Kostyshyn <dmytro.kostyshyn@magnise.com.ua>
2022-06-23 11:37:26 +0300

    Added Swagger module.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { documentOptions, swaggerOptions } from '@common/config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.setGlobalPrefix('/api');
+
+  const document = SwaggerModule.createDocument(app, documentOptions);
+  SwaggerModule.setup('', app, document, swaggerOptions);
+
   await app.listen(3000);
+  Logger.log(`Application is running on: ${await app.getUrl()}`, 'Bootstrap');
 }
 bootstrap();
